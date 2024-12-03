59884913414bd730b14198205a0d60c7f3ca5fa1
Austin Richards <austinjrichards@outlook.com>
2024-12-03 07:34:23 -0700

    Create loxinterpreter class for handling command line and error reporting, create token type enum

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import sys
 
-def run_file(file_name):
-    with open(file_name) as f:
-        read_data = f.read()
-    run(read_data)
+class LoxInterpreter:
+    def __init__(self):
+        self.had_error = False
 
-def run_prompt():
-    while True:
-        user_input = input("> ")
-        if not len(user_input):
-            break
-        run(user_input)
+    def run_file(self, file_name):
+        with open(file_name) as f:
+            read_data = f.read()
+        self.run(read_data)
+        if self.had_error:
+            sys.exit()
 
-def run():
-    scanner = Scanner()
-    tokens = scanner.scan_tokens()
+    def run_prompt(self):
+        while True:
+            user_input = input("> ")
+            if not len(user_input):
+                break
+            self.run(user_input)
 
-    # for now just print the tokens
-    for token in tokens:
-        print(token)
+    def run(self, data: str) -> None:
+        print('running file!')
+        # scanner = Scanner(data)
+        # tokens = scanner.scan_tokens()
+
+        # # for now just print the tokens
+        # for token in tokens:
+        #     print(token)
+
+    def pylox_error(self, line_no: int, message: str) -> None:
+        self.had_error = True
+        print("[line " + str(line_no) + "] Error: " + message)
+
+    def main(self):
+        if len(sys.argv) > 2:
+            print("Incorrect amount of arguments, usage: python3 main.py <lox script>")
+            sys.exit()
+        elif len(sys.argv) == 2:
+            self.run_file(sys.argv[1])
+        else:
+            self.run_prompt()
 
 def main():
-    if len(sys.argv) > 2:
-        print("Usage: jlox <script>")
-        sys.exit()
-    else if len(sys.argv) == 2:
-        run_file(sys.argv[1])
-    else:
-        run_prompt()
+    interpreter = LoxInterpreter()
+    interpreter.main()
 
 if __name__ == "__main__":
     main()
